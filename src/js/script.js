2c55989c1d99f4bc3697e4e82188b23bbff25713
Shawaiz <shawaizy2k@gmail.com>
2024-12-09 16:33:40 -0500

    Redid to fit requirements

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,28 @@
-
 // This line of code will make sure the page is fully loaded
 window.onload = function () {
-    // Write the logic in this function
+  // Write the logic in this function
 
-    // 1. Get the excuse element
-    const excuse = document.getElementById('excuse');
+  // 1. Get the excuse element
+  const excuse = document.getElementById("excuse");
 
-    // 2. Generate a random excuse
+  // 2. Generate a random excuse
 
-    // const excuseText = `The dog ate my homework for breakfast`;
-    const excuseText = [
-        "You guys suck",
-        "Sorry guys, I got wife aggro",
-        "Healer is trash",
-        "Hunter pet pulled everything",
-        "LAG!#$##%^$^"
-    ];
+  // const excuseText = `The dog ate my homework for breakfast`;
 
-    const excuseNum = Math.floor(Math.random()*5);
+  let newExcuse = [
+    ["Jake", "Allen", "My dog", "A ghost", "A rat"],
+    ["pooped at", "burned", "sold", "ate", "spat at"],
+    ["my laptop", "my computer", "my keyboard", "my phone", "my bed"],
+    ["yesterday", "a week ago", "a month ago", "a minute ago", "an hour ago"],
+  ];
 
-    // 3. Inseart the excuse into the excuse element
-    excuse.innerHTML = excuseText[excuseNum];
-}
+  let i = 0;
+  const excuseNum = Math.floor(Math.random() * 5);
+  const combinedExcuse = `${newExcuse[i++][excuseNum]} ${
+    newExcuse[i++][excuseNum]
+  } ${newExcuse[i++][excuseNum]} ${newExcuse[i++][excuseNum]}`;
+
+  // 3. Inseart the excuse into the excuse element
+  // excuse.innerHTML = excuseText[excuseNum];
+  excuse.innerHTML = combinedExcuse;
+};
